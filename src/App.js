9027718e14349da2c914eebf0ac46ad0513df768
aakarsh30 <aakarshsaxenasup@gmail.com>
2021-04-24 20:36:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import {Grid,AppBar,Toolbar,Button,makeStyles,Typography,ButtonGroup,IconButton } from '@material-ui/core';
+import {ShoppingBasket,LocalMall} from '@material-ui/icons';
+import Bgimg from './photos/key.png';
+
+const useStyles=makeStyles((theme)=>({
+  
+  
+  bdy:{
+    height: "100%",
+    
+    
+  },
+  title:{
+    color: "white",
+    flexGrow: "1"
+  },
+  btn:{
+    color:"grey",
+    '&:hover':{
+      backgroundColor: "#333333"
+    },
+    '&:focus':{
+      backgroundColor: "#333333",
+      
+    }
+    
+  },
+  header:{
+    backgroundColor: "#1d1d1f"
+  }
+})
+);
+
 
 function App() {
+  const classes=useStyles();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid className={classes.bdy} xs={12} container>
+      <Grid xs={12} item>
+        <AppBar  className={classes.header}  position="static">
+          <Toolbar>
+            <Typography className={classes.title} variant='h4'>OnlShp</Typography>
+            
+              <IconButton className={classes.btn} size="large" >
+                <LocalMall />
+              </IconButton>
+              <ButtonGroup variant="text" size="large" aria-label="text primary button group">
+              <Button className={classes.btn}>Log-in</Button>
+              <Button className={classes.btn}>Sign-up</Button>
+              
+            </ButtonGroup>
+          </Toolbar>
+        </AppBar>
+        
+      </Grid>
+    </Grid>
   );
 }
 
